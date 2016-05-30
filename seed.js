3df4249243f6b763bf30197aed293fd9aab1f00a
jenmcphail <mcphail.jen@gmail.com>
2016-05-29 17:37:25 -0700

    updates to the seed database

--- seed.js
+++ seed.js
@@ -1,15 +1,15 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var newSong = {description: "one of Jen's favorite songs."}
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
+db.Songs.create(newSong, function(err, song){
+  if (err){
+    return console.log("Error:", err);
+  }
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+  console.log("Created new song!", song._id)
+  process.exit(); // we're all done! Exit the program.
+})
